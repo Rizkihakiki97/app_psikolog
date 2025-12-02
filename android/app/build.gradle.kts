fc3923ca6d23e082b28bf6324015abc1b7f05c60
muhammdhakiki97@gmail.com <muhammdhakiki97@gmail.com>
2025-12-02 09:37:47 +0700

    harus update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
     }
   defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.app_psikolog"
+        applicationId = "com.ppkd.mindcare"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
